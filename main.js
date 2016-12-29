4c57eeaac7a2a9e55e7401653904f10342710bd7
mariangelauribe <marita.91.up@gmail.com1>
2016-12-29 09:48:04 -0500

    agregando trabajo

---
+++ main.js
@@ -0,0 +1,7 @@
+var tiempo, costoMinuto, costo; 
+tiempo=prompt("Ingrese el tiempo");
+costoMinuto=prompt("Ingrese el costo por minuto ");
+
+
+costo=parseInt(tiempo)*parseInt(costoMinuto);
+alert(costo);
